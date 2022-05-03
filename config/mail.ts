fb4bfd10d5c1ee88a2fc873c611f33f36e480e17
Abeeb <abeebayinla@gmail.com>
2022-05-03 22:46:52 +0100

    feat: added mail functionality

--- config/mail.ts
+++ config/mail.ts
@@ -1,42 +1,63 @@
+import nodemailer from 'nodemailer';
+import pug from 'pug';
+import { convert } from 'html-to-text';
 import config from 'config';
-import axios from 'axios';
 
-const mailUri: string = config.get('MAIL_URI');
-const fromEmail: string = config.get('MAIL_FROM');
-const fromName: string = config.get('MAIL_FROM_NAME');
-const key: string = config.get('SENDCHAMP_API_KEY');
-const headers = {
-  Accept: 'application/json',
-  Authorization: `Bearer ${key}`,
-  'Content-Type': 'application/json',
-};
+const mail_from:string = config.get('MAIL_FROM');
+const mail_host:string = config.get('MAIL_HOST');
+const mail_port: number = parseInt(config.get('MAIL_PORT'));
+const mail_username: string = config.get('MAIL_USERNAME');
+const mail_password: string = config.get('MAIL_PASSWORD');
 
-const sendMail = async (subject: any, to: any, message_body: any) => {
-  let data = {
-    subject,
-    to: {
-      email: to.email,
-      name: to.name,
-    },
-    from: {
-      email: fromEmail,
-      name: fromName,
-    },
-    message_body: {
-      type: 'validation',
-      value: message_body,
-    },
-  };
-  try {
-    const response = await axios(mailUri, {
-      method: 'POST',
-      data,
-      headers,
-    });
-    console.log(response);
-  } catch (err) {
-    console.log(err.message);
+export default class Email {
+  public user: string;
+  public token: any;
+  public to: string;
+  public from: string;
+  public firstName: string;
+
+  constructor(user: any, token: any) {
+    this.to = user.email;
+    this.from = `Chekkit <${mail_from}>`;
+    this.token = token;
+    this.firstName = user.name.split(' ')[0];
   }
-};
 
-export default sendMail;
+  transport() {
+    return nodemailer.createTransport({
+      host: mail_host,
+      port: mail_port,
+      secure: false,
+      auth: {
+        user: mail_username,
+        pass: mail_password,
+      },
+    });
+  }
+
+  async send(template:any, subject:string) {
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      token: this.token,
+      subject,
+    });
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html),
+    };
+
+    await this.transport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome To Chekkit');
+  }
+
+  async sendActivationMail() {
+    await this.send('activation', 'Account Successfully Activated');
+  }
+}
