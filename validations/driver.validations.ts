11c08810685d41740b59bed3006cd5cd925d644d
Abeeb <abeebayinla@gmail.com>
2022-05-03 22:50:29 +0100

    feat: added validations to driver

--- validations/driver.validations.ts
+++ validations/driver.validations.ts
@@ -47,4 +47,32 @@ export default class DriverValidation {
       });
     }
   }
+
+  async validateDriverLocation(req: Request, res: Response, next: NextFunction) {
+    const schema = Joi.object({
+      latitude: Joi.number().required().messages({
+        'number.required': 'latitude is required',
+        'number.empty': 'latitude should not be empty',
+      }),
+
+      longitude: Joi.number().required().messages({
+        'number.required': 'longitude is required',
+        'number.empty': 'longitude cannot be empty',
+      }),
+    }).options({ abortEarly: true });
+
+    try {
+      const value = await schema.validateAsync(req.body);
+      next();
+    } catch (err) {
+      let errMessage = err.details[0].message.split(' ');
+      let [field, ...others] = errMessage;
+      field = field.replace(/['"]+/g, '');
+      let newErrorMessage = `${field} ${others.join(' ')}`;
+      return res.status(422).json({
+        status: 'failure',
+        reason: newErrorMessage,
+      });
+    }
+  }
 }
